b3b356c9eb98c95aa635ee9828759ca574302b02
suneel sunkara <suneel.sunkara@gmail.com>
2021-02-23 12:29:11 +0800

    updated vm naming convention

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "azurerm_network_interface" "vmnic" {
 }
 # Create our Virtual Machine - Jonnychipz-VM01
 resource "azurerm_virtual_machine" "terraformiacvm01" {
-  name                  = "terraform-iac-vm"
+  name                  = "terraformiacvm"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.vmnic.id]
